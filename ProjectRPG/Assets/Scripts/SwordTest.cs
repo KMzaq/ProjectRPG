8dc2413532e007f6a55a79d88151231e543d6efa
KMzaq <kimminsu3544@gmail.com>
2023-01-15 07:38:52 +0900

    folderUp

---
+++ ProjectRPG/Assets/Scripts/SwordTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordTest : MonoBehaviour
+{
+    //코루틴 사용
+
+    void Start()
+    {
+        
+    }
+
+    ParticleSystem a;
+
+    void Update()
+    {
+        a.
+    }
+}
