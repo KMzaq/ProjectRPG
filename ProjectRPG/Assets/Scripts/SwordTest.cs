d6f16c829c236c7d4659f981f20b2280777f85e8
KMzaq <kimminsu3544@gmail.com>
2023-01-18 02:32:14 +0900

    test2
    test2

--- ProjectRPG/Assets/Scripts/SwordTest.cs
+++ ProjectRPG/Assets/Scripts/SwordTest.cs
@@ -2,19 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class SwordTest : MonoBehaviour
 {
-    //코루틴 사용
+    [System.Serializable]
+    public struct SwordTransform
+    {
+        public Vector3 position;
+        public Vector3 rotation;
+    }
+
+    public List<SwordTransform> sworldTransforms;
+    int curCount = 0;
+
+    bool isPlayed = false;
+
+    GameObject sword;
 
     void Start()
     {
-        
+        sword = this.gameObject;
     }
-
-    ParticleSystem a;
 
     void Update()
     {
-        a.
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Play();
+            Debug.Log(sword.transform.rotation);
+        }
+
+        if (isPlayed)
+        {
+            if (curCount == sworldTransforms.Count) isPlayed = false;
+        }
+    }
+
+    void Play()
+    {
+        if (isPlayed) return;
+        if (sworldTransforms.Count == 0) return;
+
+        curCount = 0;
+        isPlayed = true;
+        sword.transform.position = sworldTransforms[0].position;
+        sword.transform.rotation = Quaternion.Euler(sworldTransforms[0].rotation);
     }
 }
